f6f14dbdbae43f357b97ccba768e03c9965ef655
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-08-10 02:56:31 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,10 +3,10 @@
 //$user = "xkshetzj_newsarea-admins";
 //$pass = "NewsAreaMiddleEast1";
 //$db = "xkshetzj_newsarea";
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "newsarea";
+$host = "us-cdbr-east-04.cleardb.com/";
+$user = "b31560f908280d";
+$pass = "5ed34ffc";
+$db = "heroku_a92d341879b202a";
 $connect = mysqli_connect($host, $user, $pass, $db);
 mysqli_set_charset($connect, 'utf8');
 ob_start();
